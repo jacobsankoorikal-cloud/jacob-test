b0a056c5dfc363b56c7c9c01af0ccf5f2421a867
jacobsankoorikal-cloud <jacob.sankoorikal@gadgeon.com>
2026-06-23 13:51:33 +0530

    Update todo project files

---
+++ app.js
@@ -0,0 +1,8 @@
+function addTask() {
+    let task = document.getElementById("task").value;
+
+    let li = document.createElement("li");
+    li.innerHTML = task;
+
+    document.getElementById("list").appendChild(li);
+}
